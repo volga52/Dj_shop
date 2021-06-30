3722f2bfc0d0e02985020b3334ab829597ac3a85
volga52 <volga52@bk.ru>
2021-06-30 17:02:59 +0300

    view New Order

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,7 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
-from ordersapp.models import Order
+from ordersapp.forms import OrderItemEditForm
+from ordersapp.models import Order, OrderItem
 
 
 class OrderList(ListView):
@@ -11,9 +14,20 @@ class OrderList(ListView):
         return Order.objects.filter(user=self.request.user, is_active=True)
 
 
-
 class OrderCreate(CreateView):
-    pass
+    model = Order
+    success_url = reverse_lazy('order:list')
+    fields = []
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemEditForm, extra=1)
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST)
+        else:
+            formset = OrderFormSet()
+        data['orderitems'] = formset
+        return data
 
 
 class OrderUpdate(UpdateView):
