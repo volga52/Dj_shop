c28f455634f928ebf35180ff303faee175471746
volga52 <volga52@bk.ru>
2021-07-05 18:24:47 +0300

    add @receiver

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, pre_delete
+
 from basketapp.models import Basket
 from ordersapp.forms import OrderItemEditForm
 from ordersapp.models import Order, OrderItem
@@ -111,3 +114,29 @@ def forming_complete(request, pk):
     order.save()
 
     return HttpResponseRedirect(reverse('order:list'))
+
+
+# @receiver(pre_save, sender=OrderItem)
+# @receiver(pre_save, sender=Basket)
+# def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    # if update_fields is 'quantity' or 'product':
+    #     if instance.pk:
+    #         instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+    #     else:
+    #         instance.product.quantity -= instance.quantity
+    #     instance.product.save()
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def products_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if instance.pk:
+        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+    else:
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
