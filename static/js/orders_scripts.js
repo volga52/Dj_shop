2c9fa8fde67e538bcda7b052d85f1229917e813b
volga52 <volga52@bk.ru>
2021-07-06 15:55:48 +0300

    add jquery_formset

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -53,5 +53,21 @@ window.onload = function () {
         orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
     })
 
+    $('.formset_row').formset({
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        prefix: 'orderitems',
+        removed: deleteOrderItem
+    });
+
+    function deleteOrderItem(row) {
+        let target_name= row[0].querySelector('input[type="number"]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
+
+
 }
 
