19bc34cfd1688dde247ab49f99b330b92d6fbe96
evgenii <volga52@bk.ru>
2021-07-16 17:44:06 +0300

    change orderapp/views.py

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -44,7 +44,7 @@ class OrderCreate(CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
-                    form.initial['price'] = basket_items[num].price
+                    form.initial['price'] = basket_items[num].product.price
             else:
                 formset = OrderFormSet()
 
