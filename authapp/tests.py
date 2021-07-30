24c831a26be330c81d6e7529c42c1c1d7882a399
evgenii <volga52@bk.ru>
2021-07-30 16:32:27 +0300

    create test_login

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+from authapp.models import ShopUser
+
+
+class UserManagementTestCase(TestCase):
+    username = 'django'
+    email = 'django@gb.local'
+    password = 'geekbrains'
+    status_code_success = 200
+    status_code_redirect = 302
+
+    def setUp(self):
+        self.user = ShopUser.objects.create_superuser('django', email=self.email, password=self.password)
+        self.client = Client()
+
+    def test_login(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+        self.assertTrue(response.context['user'].is_anonymous)
+
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, self.status_code_redirect)
