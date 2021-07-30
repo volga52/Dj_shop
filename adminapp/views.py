60eae1d4f699df37212220e497d864b8c18f2f47
evgenii <volga52@bk.ru>
2021-07-30 08:35:07 +0300

    add discount

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,4 +1,5 @@
 from django.db import connection
+from django.db.models import F
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
@@ -104,12 +105,22 @@ class ProductCategoryUpdateView(UpdateView):
     model = ProductCategory
     template_name = 'adminapp/category_update.html'
     success_url = reverse_lazy('admin:categories')
-    fields = ('__all__')
+    # fields = ('__all__')
+    form_class = ProductCategoryEditForm
     
     def get_context_data(self, **kwargs):
         context = super(ProductCategoryUpdateView, self).get_context_data(**kwargs)
         context['title'] = 'категории/редактирование'
         return context
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (1 - discount / 100))
+                db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
+
+        return super().form_valid(form)
 
 
 class ProductCategoryDeleteView(DeleteView):
