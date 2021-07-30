3f338fba612c5ad891ccd37fd9638a68161f7649
evgenii <volga52@bk.ru>
2021-07-30 07:40:03 +0300

    update basketapp/views basket_add()

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,4 @@
+from django.db.models import F
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from basketapp.models import Basket
 from mainapp.models import Product
@@ -31,7 +32,8 @@ def basket_add(request, pk):
     old_basket_item = Basket.objects.filter(user=request.user, product=product)
     
     if old_basket_item:
-        old_basket_item[0].quantity += 1
+        # old_basket_item[0].quantity += 1
+        old_basket_item[0].quantity = F('quantity') + 1
         old_basket_item[0].save()
     else:
         new_basket_item = Basket(user=request.user, product=product)
